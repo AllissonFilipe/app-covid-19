dd116d09c6cffa68f6e7da9fd2125f0510046c6c
Farias <allisson.f.farias@accenture.com>
2020-12-05 14:13:57 -0300

    Criação do app que acompanha os casos de covid 19 a partir de uma api externa

---
+++ src/commons/constants/countries.js
@@ -0,0 +1,35 @@
+import BrazilFlag from '../../assets/images/brasil.png';
+import ChinaFlag from '../../assets/images/china.png';
+import GermanyFlag from '../../assets/images/alemanha.png';
+import ItalyFlag from '../../assets/images/italia.png';
+import UsaFlag from '../../assets/images/eua.png';
+
+const COUNTRIES = [
+    {
+        label: 'Alemanha',
+        value: 'germany',
+        // flag: GermanyFlag
+    },
+    {
+        label: 'Brasil',
+        value: 'brazil',
+        // flag: BrazilFlag
+    },
+    {
+        label: 'China',
+        value: 'china',
+        // flag: ChinaFlag
+    },
+    {
+        label: 'Estados Unidos',
+        value: 'usa',
+        // flag: UsaFlag
+    },
+    {
+        label: 'Itália',
+        value: 'italy',
+        // flag: ItalyFlag
+    }
+]
+
+export default COUNTRIES;
